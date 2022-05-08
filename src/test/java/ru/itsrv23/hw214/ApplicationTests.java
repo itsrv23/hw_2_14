cf3d7b7588e20fc84e20fd0f8a1632525714033b
itsrv23 <nifirus@gmail.com>
2022-05-08 16:52:11 +0300

    Initial commit

---
+++ src/test/java/ru/itsrv23/hw214/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.hw214;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
